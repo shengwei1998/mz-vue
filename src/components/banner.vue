94043c6b105981f267c39dd5dc22b3598496da7e
shengwei1998 <Author Email>
2019-04-10 19:33:04 +0800

    轮播图完成

---
+++ src/components/banner.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="swiper-container">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide"
+                v-for="(item,index) in banImg"
+                :key="index"
+            >
+                <img :src="item.imgUrl" alt="">
+            </div>
+        </div>
+        <!-- 如果需要分页器 -->
+        <div class="swiper-pagination"></div>
+    </div>
+</template>
+
+<script>
+const Swiper = window.Swiper;
+export default {
+    props: {
+        banImg: {
+            type: Array
+        }
+    },
+
+    data(){
+        return {
+            swiper: null
+        }
+    },
+    
+    updated(){
+        if(!this.swiper){
+            this.swiper = new Swiper('.swiper-container',{
+                loop: true,
+                autoplay: true,
+                pagination: {
+                    el: '.swiper-pagination',
+                },
+            }) 
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .swiper-container {
+        width: 100%;
+        height: 200px;
+    } 
+    .swiper-container img {
+        width: 100%;
+        height: 200px;
+    }
+    .swiper-pagination-bullet{
+        background: #fff;
+        opacity: 1;
+    }
+    .swiper-pagination-bullet-active{
+        background: orange;
+    }
+</style>
--- src/views/Film.vue
+++ src/views/Film.vue
@@ -1,5 +1,40 @@
 <template>
     <div>
-        首页
+        <banner :banImg="bannerList"></banner>
     </div>
 </template>
+
+<script>
+import axios from 'axios'
+import banner from '../components/banner'
+export default {
+    data(){
+        return {
+            bannerList: []
+        }
+    },
+    components: {
+        banner,
+    },
+    methods: {
+        getBanner(){
+            axios.get('https://m.maizuo.com/gateway?type=2&cityId=440300&k=5067437',{
+                headers: {
+                    'X-Client-Info': '{"a":"3000","ch":"1002","v":"1.0.0","e":"15546154461945620186257"}',
+                    'X-Host': 'mall.cfg.common-banner'
+                }
+            }).then(res => {
+                let data = res.data;
+                if(data.status === 0){
+                    this.bannerList = data.data;
+                } else{
+                    alert(data.msg);
+                }
+            })
+        }
+    },
+    created(){
+        this.getBanner();
+    }
+}
+</script>
